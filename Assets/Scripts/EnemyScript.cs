dd0d993d749e36c184138f07d307d368df9f94e9
Peter Cholub <pete168s@zbc.dk>
2020-06-06 16:35:51 +0200

    Added ragdoll effect and path fining

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -1,14 +1,28 @@
-﻿using UnityEngine;
-
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
 public class EnemyScript : MonoBehaviour
 {
     GameObject player;
+    GameObject destination;
     public float speed = 3f;
     public bool hit = false;
+
+    public List<Collider> ragdollParts = new List<Collider>();
+
+    public NavMeshAgent agent;
+    public Rigidbody root;
+
+    private void Awake()
+    {
+        TurnOffRagdoll();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        destination = GameObject.FindGameObjectWithTag("Destination");
     }
 
     // Update is called once per frame
@@ -20,27 +34,90 @@ public class EnemyScript : MonoBehaviour
 
     void Move()
     {
-        if (Vector3.Distance(transform.position, player.transform.position) > 0.3f)
+        agent.SetDestination(destination.transform.position);
+    }
+
+    void TurnOffRagdoll()
+    {
+        Collider[] colliders = gameObject.GetComponentsInChildren<Collider>();
+
+        foreach (Collider c in colliders)
         {
-            transform.LookAt(transform.position - player.transform.position);
-            float step = speed * Time.deltaTime; // calculate distance to move
-            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, step);
-            //transform.LookAt(player.transform.position,Vector3.up);
-            // Check if the position of the cube and sphere are approximately equal.
-
-
+            if (c.gameObject != this.gameObject)
+            {
+                c.attachedRigidbody.isKinematic = true;
+                c.isTrigger = true;
+                ragdollParts.Add(c);
+            }
         }
     }
 
+    void TurnOnRagdoll()
+    {
+        gameObject.GetComponent<Collider>().enabled = false;
+
+        foreach (Collider c in ragdollParts)
+        {
+            c.attachedRigidbody.isKinematic = false;
+            //c.attachedRigidbody.velocity = Vector3.zero;
+            c.isTrigger = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Hit");
+
+        if(collision.gameObject.tag == "Destination")
+        {
+            TurnOnRagdoll();
+
+            Vector3 force =  transform.position - collision.transform.position;
+
+            //RotationScript rotation = GetComponentInChildren<RotationScript>();
+
+            force.Normalize();
+
+            agent.isStopped = true;
+
+            //float magnitude = collision.gameObject.GetComponentInChildren<ResenganScript>().magnitude;
+            Debug.Log(force);
+
+            Rigidbody[] body = gameObject.GetComponentsInChildren<Rigidbody>();
+
+            foreach (Rigidbody r in body)
+            {
+                r.AddForce(1000f * force);
+            }
+
+            //other.gameObject.transform.Rotate(rotation.rotation * Time.deltaTime * 30, Space.Self);
+
+            hit = true;
+        }
+    }
 
     private void OnParticleCollision(GameObject other)
     {
         Vector3 force = transform.position - other.transform.position;
 
+        //RotationScript rotation = GetComponentInChildren<RotationScript>();
+        TurnOnRagdoll();
+
+
         force.Normalize();
-        
-        gameObject.GetComponent<Rigidbody>().freezeRotation = false;
-        gameObject.GetComponent<Rigidbody>().AddForce(force * 10 * speed);
+
+        agent.isStopped = true;
+
+        //float magnitude = collision.gameObject.GetComponentInChildren<ResenganScript>().magnitude;
+        Debug.Log(force);
+
+        Rigidbody[] body = gameObject.GetComponentsInChildren<Rigidbody>();
+
+        foreach (Rigidbody r in body)
+        {
+            r.AddForce(1300f * force);
+        }
+
 
         hit = true;
     }
